29e0a558d1fcb1cae4627aa33255f5fb3c9463f0
sukri <sukriy188@gmail.com>
2020-10-10 05:48:26 +0700

    create route url

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,42 @@
 <?php
-    include 'app/Vehicles.php';
-    $data = new Vehicles;
+include 'app/Vehicles.php';
+
+$data = new Vehicles;
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$baseUrl = '/WebProjects/backend';
+
+switch ($request) 
+{
+    case $baseUrl.'/' :
+        if($_SERVER['REQUEST_METHOD'] != "GET"){
+            echo json_encode([
+                'success' => false,
+                'message' => 'not support request method'
+            ]);
+            break;
+        }
+        echo json_encode($data->retrieve($_GET));
+        break;
+    case $baseUrl.'/reset' :
+        if($_SERVER['REQUEST_METHOD'] != "GET"){
+            echo json_encode([
+                'success' => false,
+                'message' => 'not support request method'
+            ]);
+            break;
+        }
+        echo json_encode($data->reset());
+        break;
+    case $baseUrl.'/store' :
+        if($_SERVER['REQUEST_METHOD'] != "POST"){
+            echo json_encode([
+                'success' => false,
+                'message' => 'not support request method'
+            ]);
+            break;
+        }
+        echo json_encode($data->store($_POST));
+        break;
+    default:
+        break;
+}
